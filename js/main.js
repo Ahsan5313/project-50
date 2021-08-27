aabe4e5e40160b6107cf553ae43f8a77b77f66f3
Freelancer Ahsan <81156921+Ahsan5313@users.noreply.github.com>
2021-08-27 23:44:23 +0600

    E-commerce webste
    This is an E-commerce resposive website. I use for this website like HTML CSS JAVASCRIPT and BOOTSTRAP. This is also a responsive website. For responsive i used media query

---
+++ js/main.js
@@ -0,0 +1,27 @@
+setTimeout (() => {
+
+    document.getElementById('loader').style.display = 'none'
+}, 2000)
+
+const myBtn = document.getElementById('myBtn')
+
+window.onscroll = function(){scrollFunction()}
+
+function scrollFunction(){
+
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+
+        myBtn.style.display = 'block';
+    }
+    else{
+
+        myBtn.style.display = 'none';
+    }
+}
+
+
+function topFunction() {
+
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
